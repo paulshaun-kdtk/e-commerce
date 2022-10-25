2787887385800606a5b3c5f4dee0f633b475dc28
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-10-25 17:10:09 +0200

    Update store.js

--- client/src/store.js
+++ client/src/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, combineReducers } from 'redux';
+import { createStoe, applyMiddleware, combineReducers } from 'redux';
 import thunk from 'redux-thunk';
 import {
   listProducts,
