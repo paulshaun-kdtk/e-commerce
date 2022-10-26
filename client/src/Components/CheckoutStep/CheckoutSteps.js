03e2952c2259c6aaf25ce17b0521305b2e0eadc8
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-10-26 22:36:46 +0200

    Update CheckoutSteps.js

--- client/src/Components/CheckoutStep/CheckoutSteps.js
+++ client/src/Components/CheckoutStep/CheckoutSteps.js
@@ -1,4 +1,4 @@
-import React fr 'react';
+import React f 'react';
 import { Nav } from 'react-bootstrap';
 import * as routes from '../../Constants/routes';
 import { LinkContainer } from 'react-router-bootstrap';
