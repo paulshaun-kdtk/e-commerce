afa16185c6ab9da2f36fbc8ec9cde968473fe088
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-10-29 21:40:00 +0200

    Update cartAction.js

--- client/src/Actions/cartAction.js
+++ client/src/Actions/cartAction.js
@@ -55,5 +55,5 @@ export const savePaymentMethod = (data) => async (dispatch) => {
     payload: data,
   });
 
-  localStor.setItem('paymentMethod', JSON.stringify(data));
+  localStr.setItem('paymentMethod', JSON.stringify(data));
 };
