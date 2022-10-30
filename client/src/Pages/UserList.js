e39167f3e901822cc7a515f06a8b575927264beb
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-10-30 12:34:28 +0200

    Update UserList.js

--- client/src/Pages/UserList.js
+++ client/src/Pages/UserList.js
@@ -1,4 +1,4 @@
-impo React, { useEffect, useState } from 'react';
+impor React, { useEffect, useState } from 'react';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Table, Button } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
