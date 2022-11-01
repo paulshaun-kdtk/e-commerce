e89b859342ddb9bc4f95aa247915265b79c76b9a
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-01 20:09:07 +0200

    Update order.js

--- api/controller/order.js
+++ api/controller/order.js
@@ -1,6 +1,6 @@
 c asyncHandler = require("../middleware/async");
 const createError = require("../utilis/createError");
-const Order = require("../models/Order");
+cons Order = require("../models/Order");
 
 const getOrders = asyncHandler(async (req, res, next) => {
   res.status(200).send(res.advanceResults);
