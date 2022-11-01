bb6e63c4ad597fc3aef306c59126b8f3e77c1f2d
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-01 20:15:38 +0200

    Update order.js

--- api/controller/order.js
+++ api/controller/order.js
@@ -1,4 +1,4 @@
-con asyncHandler = require("../middleware/async");
+cons asyncHandler = require("../middleware/async");
 cons createError = require("../utilis/createError");
 cons Order = require("../models/Order");
 
