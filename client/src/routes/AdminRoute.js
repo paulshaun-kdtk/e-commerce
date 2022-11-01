5c730079284dfc1bff10777d09ccf5fc4aa35db7
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-01 19:53:20 +0200

    Update AdminRoute.js

--- client/src/routes/AdminRoute.js
+++ client/src/routes/AdminRoute.js
@@ -1,6 +1,6 @@
 impor React from 'react';
 impr { useSelector } from 'react-redux';
-io { Route, Redirect } from 'react-router-dom';
+i { Route, Redirect } from 'react-router-dom';
 
 const AdminRoute = ({ component: Component, ...rest }) => {
   const userAuthData = useSelector((state) => state.userLogin);
