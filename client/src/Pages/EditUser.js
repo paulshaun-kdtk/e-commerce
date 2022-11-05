6ebf6c78c189edbf99a1965ee81e520803fecfb6
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-05 20:46:54 +0200

    Update EditUser.js

--- client/src/Pages/EditUser.js
+++ client/src/Pages/EditUser.js
@@ -1,4 +1,4 @@
-import Re, { useState, useEffect } from 'react';
+import Rea, { useState, useEffect } from 'react';
 import { Form } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import FormContainer from '../Components/FormContainer/FormContainer';
