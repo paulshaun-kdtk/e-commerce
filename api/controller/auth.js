dba0e3a76fc9877676a9a4ceec2504205bad94b5
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-13 23:23:26 +0200

    Update auth.js

--- api/controller/auth.js
+++ api/controller/auth.js
@@ -4,7 +4,7 @@ const asyncHandler = require("../middleware/async");
 const verifyEmail = require("../utilis/verifyEmail");
 const sendEmail = require("../utilis/sendEmail");
 const cron = require("node-cron");
-const User = require("../models/User");
+cont User = require("../models/User");
 
 const RegisterUser = asyncHandler(async (req, res, next) => {
   var uid = "";
