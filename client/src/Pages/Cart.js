f3f3aa9e8dd37bb43749d225bdad589194b321a4
Nyasha Karata <91337717+NyashaKarata@users.noreply.github.com>
2022-11-16 21:25:25 +0200

    Update Cart.js

--- client/src/Pages/Cart.js
+++ client/src/Pages/Cart.js
@@ -1,4 +1,4 @@
-i React from 'react';
+im React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { Row, Col, ListGroup, Image, Card } from 'react-bootstrap';
